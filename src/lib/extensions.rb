44df7933883bd561e1a88e89755cb830d615f019
Jon-Michael Deldin <dev@jmdeldin.com>
2012-12-09 22:28:29 -0700

    Add Array#remove_stop_words

--- src/lib/extensions.rb
+++ src/lib/extensions.rb
@@ -1,5 +1,13 @@
+require_relative 'vendor/ankusa/lib/ankusa/stopwords'
+
 class String
   def to_words
     scan(/[a-z']/i)
+  end
+end
+
+class Array
+  def remove_stops
+    reject { |w| Ankusa::STOPWORDS.include?(w) }
   end
 end
